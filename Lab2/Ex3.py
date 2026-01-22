3a27946ded474ee63ee5829ff3c6f917da7137cf
nazcat532 <taniguchinazca@gmail.com>
2026-01-22 13:01:26 -1000

    Update Ex3.py

--- Lab2/Ex3.py
+++ Lab2/Ex3.py
@@ -1,2 +1,15 @@
 #Ask the user to enter a floating point number. Square the number
 # Print out the orignal number and the squared result 
+# Name: Nazca
+# Date: 2026-01-22
+
+input_value = input("Please enter a floating point number: ")
+float
+squared_value = float(input_value) ** 2
+
+# Round te number to 2 decimal places
+squared_value = round(squared_value, 2)
+
+printI("You entered:", float_value)
+print("The squared value is:", squared_value)
+
