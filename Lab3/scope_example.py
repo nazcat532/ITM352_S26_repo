de5a53f1a87dbf0f3a290dfb20c57a39dbc5f2b8
nazcat532 <taniguchinazca@gmail.com>
2026-01-27 12:33:26 -1000

    Update scope_example.py

--- Lab3/scope_example.py
+++ Lab3/scope_example.py
@@ -3,12 +3,14 @@
 # Date: 2026-01-27
 
 def calculate_discounted_price(price):
+    #global discount 
     discount = 0.9
-    price = price*discount
+    price = price * discount
     price(f"Inside function, discounted price: {price:.2f}")
     return price
 
-price = 100
+discount = 0.6
+price = 100.0
 price(f"Original price before function call: {price:.2f}")
 discounted_price = calculate_discounted_price(price)
 
